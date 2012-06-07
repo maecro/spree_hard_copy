3bc81a03d5832686e636b625775071e60f7854fb
Matthew Cross <matthew.evan.cross@gmail.com>
2012-06-08 04:07:45 +1000

    Changed insert position.

--- app/overrides/add_print_button.rb
+++ app/overrides/add_print_button.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/orders/show",
                      :name => "add_print_button",
-                     :insert_after => "[data-hook='links']",
+                     :insert_bottom => "[data-hook='links']",
                      :text => "<%= link_to 'Print Invoice', '/', :class => 'button' %>",
                      :disabled => false)

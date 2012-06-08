bdec92e704ac90870ce3ae8260070a04733e40aa
Matthew Cross <matthew.evan.cross@gmail.com>
2012-06-09 01:25:45 +1000

    Bug hunting.

--- app/overrides/add_print_button.rb
+++ app/overrides/add_print_button.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/orders/show",
                      :insert_bottom => "[data-hook='links']",
-                     :text => "<%= link_to 'Print Invoice', '/invoice/#{@order}', :class => 'button' %>",
+                     :text => "<%= link_to 'Print Invoice', '/invoice/#{@order.number}', :class => 'button' %>",
                      :name => "print_invoice")

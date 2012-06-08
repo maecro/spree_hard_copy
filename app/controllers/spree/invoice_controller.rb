45b799f781939b12cc5c349353ddcb0eeb947b5c
Matthew Cross <matthew.evan.cross@gmail.com>
2012-06-09 00:16:16 +1000

    Bug hunting.

--- app/controllers/spree/invoice_controller.rb
+++ app/controllers/spree/invoice_controller.rb
@@ -1,12 +1,10 @@
 module Spree
-  module Admin
-    class InvoiceController < Spree::BaseController
-      def lasku
-        template = params[:template]
-        eval "@#{template} = true"
-        @order = Spree::Order.find_by_number(params[:id])
-        render  template , :layout => false
-      end
+  class InvoiceController < Spree::BaseController
+    def lasku
+      template = params[:template]
+      eval "@#{template} = true"
+      @order = Spree::Order.find_by_number(params[:id])
+      render  template , :layout => false
     end
   end
 end
--- app/overrides/add_print_button.rb
+++ app/overrides/add_print_button.rb
@@ -1,5 +1,3 @@
 Deface::Override.new(:virtual_path => "spree/orders/show",
-                     :name => "add_print_button",
                      :insert_bottom => "[data-hook='links']",
-                     :text => "<%= link_to 'Print Invoice', '/invoice/#{@order.number}', :class => 'button' %>",
-                     :disabled => false)
+                     :text => "<%= link_to 'Print Invoice', '/invoice/#{@order}', :class => 'button' %>")

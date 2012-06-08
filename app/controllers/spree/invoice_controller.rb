cf8dc52b4ca685a5d54407c6efc6d7f5a7d6a0ad
Matthew Cross <matthew.evan.cross@gmail.com>
2012-06-09 09:23:08 +1000

    Bug hunting.

--- app/controllers/spree/invoice_controller.rb
+++ app/controllers/spree/invoice_controller.rb
@@ -2,7 +2,7 @@ module Spree
   class InvoiceController < Spree::BaseController
     def lasku
       template = params[:template]
-      eval "@#{template} = true"
+      #eval "@#{template} = true"
       @order = Spree::Order.find_by_number(params[:id])
       render  template , :layout => false
     end
